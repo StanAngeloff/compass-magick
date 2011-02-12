1715a6f7510d1fc171b13807860c5571bc243dd4
Stan Angeloff <stanimir@psp-webtech.co.uk>
2011-02-12 21:16:01 +0200

    When converting numbers from Sass, return default value when boolean given instead.
    This allows .scss authors to skip over function arguments and only pass in the desired parameters.

--- lib/compass-magick/util.rb
+++ lib/compass-magick/util.rb
@@ -1,6 +1,6 @@
 module Compass::Magick::Util
   def self.number_value(number, length, default = nil)
-    return default if number.nil?
+    return default if number.nil? || number.is_a?(Sass::Script::Bool)
     return length * (number.value.to_f / 100) if number.unit_str === '%'
     number.value
   end
