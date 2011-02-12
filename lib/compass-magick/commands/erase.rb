e7180574324b9fedf9680f5ccda1c5cbd80dbe32
Stan Angeloff <stanimir@psp-webtech.co.uk>
2011-02-12 21:15:11 +0200

    Rename width / height to x2 / y2 as this is more accurate.

--- lib/compass-magick/commands/erase.rb
+++ lib/compass-magick/commands/erase.rb
@@ -1,21 +1,21 @@
 module Compass::Magick::Commands
   class Erase < Compass::Magick::Command
-    def initialize(color, x = nil, y = nil, width = nil, height = nil)
-      @color  = color
-      @x      = x
-      @y      = y
-      @width  = width
-      @height = height
+    def initialize(color, x1 = nil, y1 = nil, x2 = nil, y2 = nil)
+      @color = color
+      @x1    = x1
+      @y1    = y1
+      @x2    = x2
+      @y2    = y2
     end
 
     def invoke(image)
       draw = Magick::Draw.new
       draw.fill = @color.to_s
       draw.rectangle(
-        Compass::Magick::Util.number_value(@x,      image.columns, 0),
-        Compass::Magick::Util.number_value(@y,      image.rows,    0),
-        Compass::Magick::Util.number_value(@width,  image.columns, image.columns),
-        Compass::Magick::Util.number_value(@height, image.rows,    image.rows)
+        Compass::Magick::Util.number_value(@x1, image.columns, 0),
+        Compass::Magick::Util.number_value(@y1, image.rows,    0),
+        Compass::Magick::Util.number_value(@x2, image.columns, image.columns),
+        Compass::Magick::Util.number_value(@y2, image.rows,    image.rows)
       )
       draw.draw(image)
     end
