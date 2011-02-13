288d4204f1124daa4a33bbaa27f86c9279e6571e
Stan Angeloff <stanimir@psp-webtech.co.uk>
2011-02-13 14:32:38 +0200

    Add support for negative positions so '-10' translates to `width - 10`.
    You can now clear an image and leave a 1px edge by using:
    
        magick-erase(red, 1, 1, -1, -1)

--- lib/compass-magick/util.rb
+++ lib/compass-magick/util.rb
@@ -2,6 +2,7 @@ module Compass::Magick::Util
   def self.number_value(number, length, default = nil)
     return default if number.nil? || number.is_a?(Sass::Script::Bool)
     return length * (number.value.to_f / 100) if number.unit_str === '%'
+    return length + number.value if number.value < 0
     number.value
   end
 end
