234ea9063958b615d0cb0e5d820cebd23c427395
Stan Angeloff <stanimir@psp-webtech.co.uk>
2011-02-20 17:34:09 +0200

    Whoops, we sort of broke Sass by including the Utils module in Drawing.

--- lib/magick/functions/drawing.rb
+++ lib/magick/functions/drawing.rb
@@ -2,8 +2,6 @@ module Compass::Magick
   module Functions
     # Methods for drawing on a {Compass::Magick::Canvas}.
     module Drawing
-      include Utils
-
       # Fills the {Canvas} region with the given {Compass::Magick::Type}.
       #
       # @param [Object] type The type of fill to apply. Supported:
@@ -13,10 +11,10 @@ module Compass::Magick
       # @return {Command} A command which applies the fill on the canvas.
       def magick_fill(type, x1 = nil, y1 = nil, x2 = nil, y2 = nil)
         Command.new do |canvas|
-          canvas_x1 = value_of(x1, canvas.width  - 1, 0)
-          canvas_y1 = value_of(y1, canvas.height - 1, 0)
-          canvas_x2 = value_of(x2, canvas.width  - 1, canvas.width - 1)
-          canvas_y2 = value_of(y2, canvas.height - 1, canvas.height - 1)
+          canvas_x1 = Compass::Magick::Utils.value_of(x1, canvas.width  - 1, 0)
+          canvas_y1 = Compass::Magick::Utils.value_of(y1, canvas.height - 1, 0)
+          canvas_x2 = Compass::Magick::Utils.value_of(x2, canvas.width  - 1, canvas.width - 1)
+          canvas_y2 = Compass::Magick::Utils.value_of(y2, canvas.height - 1, canvas.height - 1)
           width     = Sass::Script::Number.new(canvas_x2 - canvas_x1 + 1)
           height    = Sass::Script::Number.new(canvas_y2 - canvas_y1 + 1)
           if type.kind_of?(Sass::Script::Color)
