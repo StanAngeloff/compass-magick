25a63e413a2dc16ec2129c5ef70d06bf044322a9
Stan Angeloff <stanimir@psp-webtech.co.uk>
2011-02-20 13:16:05 +0200

    Make Compass::Magick::Canvas inherit from ChunkyPNG::Canvas.
    We were using Sass::Script::Funcall previously, but since we will be doing a lot of operations on the Canvas, it is easier to extend Chunky.

--- lib/magick/canvas.rb
+++ lib/magick/canvas.rb
@@ -1,18 +1,18 @@
 require 'base64'
 
 module Compass::Magick
-  # A Canvas class that uses ChunkyPNG::Canvas internally to represent the
-  # image as a matrix of pixels.
+  # A Canvas class that inherits ChunkyPNG::Canvas to represent the image as
+  # a matrix of pixels.
   #
-  # The image is constructed from a given width and height on a transparent
+  # The canvas is constructed from a given width and height on a transparent
   # background. The list of commands is executed in order and the resulting
   # image is returned as a Base64 encoded PNG-24 Data URI.
   #
   # @see http://rdoc.info/gems/chunky_png/0.12.0/ChunkyPNG/Canvas
   # @example
   #
-  #     background: transparent magick-canvas(320, 200);
-  class Canvas < Sass::Script::Funcall
+  #     Canvas.new(320, 240).to_data_uri
+  class Canvas < ChunkyPNG::Canvas
     include Compass::Magick::Utils
 
     # Initializes a new Canvas instance.
@@ -24,8 +24,7 @@ module Compass::Magick
     def initialize(width, height, *commands)
       assert_type 'width',  width,  Sass::Script::Number
       assert_type 'height', height, Sass::Script::Number
-      super('magick-canvas', [width, height] + commands)
-      @image = ChunkyPNG::Canvas.new(width.value, height.value)
+      super(width.value, height.value)
     end
 
     # Serialize the Canvas as a Base64 encoded PNG-24 Data URI.
@@ -33,7 +32,7 @@ module Compass::Magick
     # @return [String] A Base64 encoded PNG-24 Data URI for the generated
     #   image.
     def to_data_uri
-      data = Base64.encode64(@image.to_blob).gsub("\n", '')
+      data = Base64.encode64(to_blob).gsub("\n", '')
       Sass::Script::String.new("url('data:image/png;base64,#{data}')")
     end
 
