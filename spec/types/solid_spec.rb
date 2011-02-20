5cd554388efe9602caa86a07490baa9df2b5a466
Stan Angeloff <stanimir@psp-webtech.co.uk>
2011-02-20 15:15:07 +0200

    Improve Solid fill spec by checking the top-left and bottom-right corners of the generated image.

--- spec/types/solid_spec.rb
+++ spec/types/solid_spec.rb
@@ -12,6 +12,8 @@ describe Compass::Magick::Types::Solid do
   it { should respond_to(:color) }
 
   it "should generate a solid Canvas" do
-    subject.to_canvas(Sass::Script::Number.new(100), Sass::Script::Number.new(100)).get_pixel(0, 0).should == ChunkyPNG::Color.rgba(255, 0, 0, 255)
+    canvas = subject.to_canvas(Sass::Script::Number.new(100), Sass::Script::Number.new(100))
+    canvas.get_pixel( 0,  0).should == ChunkyPNG::Color.rgba(255, 0, 0, 255)
+    canvas.get_pixel(99, 99).should == ChunkyPNG::Color.rgba(255, 0, 0, 255)
   end
 end
