1f95a5c6f4761d205dd5c6213f44769de1082761
Stan Angeloff <stanimir@psp-webtech.co.uk>
2011-03-13 20:39:43 +0200

    Add 'brightness' effect (borrowed from CamanJS).

--- lib/magick/functions/effects.rb
+++ lib/magick/functions/effects.rb
@@ -5,16 +5,39 @@ module Compass::Magick
     module Effects
       extend self
 
-      # Lowers the intensity of a color, by lowering its alpha by a given
-      # factor.
+      # Adjusts the intensity of a color by changing its alpha by a given
+      # value.
       #
-      # @param [Sass::Script::Number] factor Fade factor as a float between
+      # @param [Sass::Script::Number] adjust Fade value as a float between
       #   0.0 and 1.0.
       # @return {Effect} A command which applies the fade to the canvas.
-      def fade(factor = nil)
-        Compass::Magick::Utils.assert_type 'factor', factor, Sass::Script::Number
-        fade_factor = 255 - (255 * Compass::Magick::Utils.value_of(factor, 1.0, 0.5)).to_i
-        Effect.new { |pixel| ChunkyPNG::Color.fade(pixel, fade_factor) }
+      def fade(adjust = nil)
+        Compass::Magick::Utils.assert_type 'adjust', adjust, Sass::Script::Number
+        fade_adjust = 255 - (255 * Compass::Magick::Utils.value_of(adjust, 1.0, 0.5)).to_i
+        Effect.new { |pixel| ChunkyPNG::Color.fade(pixel, fade_adjust) }
+      end
+
+      # Adjusts the brightness of a color by changing its [R, G, B]
+      # components by a given value.
+      #
+      # Copyright (c) 2010, Ryan LeFevre
+      # http://www.camanjs.com
+      #
+      # @param [Sass::Script::Number] adjust Brightness value as a float
+      #   between -1.0 and 1.0.
+      # @return {Effect} A command which applies the Brightness to the
+      #   canvas.
+      def brightness(adjust = nil)
+        Compass::Magick::Utils.assert_type 'adjust', adjust, Sass::Script::Number
+        brightness_adjust = (255 * Compass::Magick::Utils.value_of(adjust, 1.0, 0.5)).to_i
+        Effect.new do |pixel|
+          ChunkyPNG::Color.rgba(
+            [0, [ChunkyPNG::Color.r(pixel) + brightness_adjust, 255].min].max,
+            [0, [ChunkyPNG::Color.g(pixel) + brightness_adjust, 255].min].max,
+            [0, [ChunkyPNG::Color.b(pixel) + brightness_adjust, 255].min].max,
+            ChunkyPNG::Color.a(pixel)
+          )
+        end
       end
     end
   end
