b36b5aaff8316124f394cfb4362f90c072da31c3
Nick Dima <nick.dima@gmail.com>
2012-04-10 13:03:58 +0200

    Update PhantomJS support script to work with the recent versions of PhantomJS

--- extras/magick.js
+++ extras/magick.js
@@ -1,57 +1,51 @@
 #!/usr/bin/env phantomjs
-if (phantom.state.length === 0) {
-  var width   = parseInt(phantom.args[0], 10),
-      height  = parseInt(phantom.args[1], 10),
-      padding = Math.max(width, height),
-      head, body, i, styles, j, length, instruction;
-  if (isNaN(width) || isNaN(height) || phantom.args.length < 4) {
-    console.error('Usage: magick.js width height styles [styles […]] filename');
-    phantom.exit();
-  } else {
-    head = []; body = [];
-    for (i = 2; i < phantom.args.length - 1; i ++) {
-      styles = phantom.args[i].split(';');
-      for (j = 0, length = styles.length; j < length; j ++) {
-        instruction = styles[j].replace(/^\s+|\s+$/g, '');
-        if (instruction.indexOf(':') > 0) {
-          styles.push('-webkit-' + instruction);
-        }
-      }
-      head.push('#element-' + i + ' { ' +
-        'width:  ' + width  + 'px; ' +
-        'height: ' + height + 'px; ' +
-        'box-sizing: border-box; ' +
-        '-webkit-box-sizing: border-box; ' +
-        styles.join('; ') + '; ' +
-      '}');
-      body.push('<div id="element-' + i + '"></div>');
-    }
-    try {
-      phantom.state = 1;
-      phantom.viewportSize = {
-        width:  width  + padding * 2,
-        height: height + padding * 2
-      };
-      phantom.content =
-        '<html>' +
-          '<head>' +
-          '<style>' +
-            head.join('\n') +
-          '</style>' +
-          '</head>' +
-          '<body style="background: transparent; margin: ' + padding + 'px ' + padding + 'px; padding: 0;">' +
-            body.join('\n') +
-          '</body>' +
-        '</html>';
-    } catch (e) {
-      phantom.exit();
-      throw e;
-    }
-  }
+var width   = parseInt(phantom.args[0], 10),
+    height  = parseInt(phantom.args[1], 10),
+    padding = Math.max(width, height),
+    page = require('webpage').create(),
+    head, body, i, styles, j, length, instruction;
+if (isNaN(width) || isNaN(height) || phantom.args.length < 4) {
+  console.error('Usage: magick.js width height styles [styles […]] filename');
+  phantom.exit();
 } else {
+  head = []; body = [];
+  for (i = 2; i < phantom.args.length - 1; i ++) {
+    styles = phantom.args[i].split(';');
+    for (j = 0, length = styles.length; j < length; j ++) {
+      instruction = styles[j].replace(/^\s+|\s+$/g, '');
+      if (instruction.indexOf(':') > 0) {
+        styles.push('-webkit-' + instruction);
+      }
+    }
+    head.push('#element-' + i + ' { ' +
+      'width:  ' + width  + 'px; ' +
+      'height: ' + height + 'px; ' +
+      'box-sizing: border-box; ' +
+      '-webkit-box-sizing: border-box; ' +
+      styles.join('; ') + '; ' +
+    '}');
+    body.push('<div id="element-' + i + '"></div>');
+  }
   try {
-    phantom.render(phantom.args[phantom.args.length - 1]);
-  } finally {
+    page.viewportSize = {
+      width:  width  + padding * 2,
+      height: height + padding * 2
+    };
+    page.content =
+      '<html>' +
+        '<head>' +
+        '<style>' +
+          head.join('\n') +
+        '</style>' +
+        '</head>' +
+        '<body style="background: transparent; margin: ' + padding + 'px ' + padding + 'px; padding: 0;">' +
+          body.join('\n') +
+        '</body>' +
+      '</html>';
+    page.render(phantom.args[phantom.args.length - 1]);
     phantom.exit();
+  } catch (e) {
+    phantom.exit();
+    throw e;
   }
 }
